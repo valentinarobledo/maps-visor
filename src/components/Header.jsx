de7f594860157a6aac6954675098bc8850990ef7
valentinarobledo <valenrobledo31@gmail.com>
2023-10-20 17:55:48 -0500

    Testing url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='py-1 px-3 mb-2 bg-gris header'>
-        <h3 className='color-white'>Visor App</h3>
+        <h3 className='color-white'>Visor Apppp</h3>
     </div>
   )
 }
